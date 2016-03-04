f0ca2fba1ff305a3eb51a71062861272cbd400b7
JessBn <jessbnmd@gmail.com>
2016-03-04 21:10:55 +0100

    conn

---
+++ src/main/java/dam/freshlook/services/Conexion.java
@@ -0,0 +1,40 @@
+
+package dam.freshlook.services;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+public class Conexion {
+	private static final String BD_IP = "localhost";
+
+	
+	private static Conexion instance = null;
+	MongoClient mongo;
+	DB db;
+
+	private Conexion() {
+		mongo = crearConexion();
+		if (mongo != null) {
+			db = mongo.getDB("FreshLook");
+		}
+	}
+	
+	public static Conexion getInstance(){
+		if(instance == null) {
+	         instance = new Conexion();
+	      }
+	      return instance;
+	}
+
+	public DB getDb() {
+		return db;
+	}
+
+
+	private static MongoClient crearConexion() {
+		MongoClient mongo = null;
+		mongo = new MongoClient(BD_IP, 27017);
+		return mongo;
+	}
+}
